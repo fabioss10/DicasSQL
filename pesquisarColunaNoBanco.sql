39178914e9c38149d59c06c2275597d2ee03257f
fabioss10 <fabioss10@hotmail.com>
2021-02-25 14:49:00 -0300

    Add files via upload

---
+++ pesquisarColunaNoBanco.sql
@@ -0,0 +1,14 @@
+SELECT 
+	T.name AS Tabela, 
+	C.name AS Coluna
+FROM 
+	sys.sysobjects    AS T (NOLOCK) 
+INNER JOIN sys.all_columns AS C (NOLOCK) ON T.id = C.object_id AND T.XTYPE = 'U' 
+WHERE 
+	C.NAME LIKE '%idsistema%'
+ORDER BY 
+	T.name ASC
+
+	
+	
+	
